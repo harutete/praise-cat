487511ed27f05bcedfd69d6e18fcc09ed5e42830
harutete <ai.takeuchi.10@gmail.com>
2020-05-07 14:08:59 +0900

    Fix: カレンダーとh2をコンポーネント化する

---
+++ src/components/Calender/index.tsx
@@ -0,0 +1,100 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+
+//Component
+import Heading02 from '../atoms/Heading02'
+
+const Calender = () => {
+  const fetchCurrentDate = () => {
+    const date = new Date()
+    const year = date.getFullYear()
+    const month = date.getMonth() + 1
+
+    return `${year}/${month}`
+  }
+  const drawingCalendar = (year?: number, month?: number): (string | number)[][] => {
+    const date = new Date()
+    const currentYear = year || date.getFullYear()
+    const currentMonth = month ? month - 1 : date.getMonth()
+    const startDate = new Date(currentYear, currentMonth, 1)
+    const startWeekDay = startDate.getDay()
+    const endDate = new Date(currentYear, currentMonth + 1, 0)
+    const endDay = endDate.getDate()
+    const dateArr = []
+    let row
+    let col
+    let day = 1
+
+    for (row = 0; row < 6; row++) {
+      const weekArr = []
+      for(col = 0; col < 7; col++) {
+        if (row === 0 && startWeekDay > col) {
+          weekArr.push('')
+        } else if (day > endDay) {
+          weekArr.push('')
+          day++
+        } else {
+          weekArr.push(day)
+          day++
+        }
+      }
+
+      dateArr.push(weekArr)
+    }
+
+    return dateArr
+  }
+  const date = fetchCurrentDate()
+  const currentCalendar = drawingCalendar()
+  const [ currentDate, setCurrentDate ] = useState(date)
+  const [ calender, setCalendar ] = useState(currentCalendar)
+  const reRenderCalender = (event: any) => {
+    const state = event.target.getAttribute('data-pagenation')
+    const dateArr = currentDate.split('/').map(date => parseInt(date, 10))
+    if (state === 'prev') {
+      const year = dateArr[1] === 1 ? dateArr[0] -1 : dateArr[0]
+      const month = dateArr[1] === 1 ? 12 : dateArr[1] - 1
+      const renderCalender = drawingCalendar(year, month)
+      setCurrentDate(`${year}/${month}`)
+      setCalendar(renderCalender)
+    } else {
+      const year = dateArr[1] === 12 ? dateArr[0] + 1 : dateArr[0]
+      const month = dateArr[1] === 12 ? 1 : dateArr[1] + 1
+      const renderCalender = drawingCalendar(year, month)
+      setCurrentDate(`${year}/${month}`)
+      setCalendar(renderCalender)
+    }
+  }
+  const weekDayArr = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+
+  return (
+    <div>
+      <Heading02 text="がんばった一覧" />
+      <table>
+        <caption>{currentDate}</caption>
+        <thead>
+          <tr>
+            {weekDayArr.map((day, index) => <th key={`weekday_${index}`}>{day}</th>)}
+          </tr>
+        </thead>
+        <tbody>
+          {calender.map((week, index) =>
+            <tr key={`week_${index}`}>
+              {week.map((day, index) => <td key={`day_${index}`}>{day}</td>)}
+            </tr>
+          )}
+        </tbody>
+      </table>
+      <ul>
+        <li>
+          <button type="button" onClick={reRenderCalender} data-pagenation="prev">Prev</button>
+        </li>
+        <li>
+          <button type="button" onClick={reRenderCalender} data-pagenation="next">Next</button>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Calender
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,11 +1,13 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { ThemeProvider } from 'styled-components'
 import { theme } from '../components/theme'
 // components
 import GlobalStyle from '../components/FoundationStyle'
 import HeaderContent from '../components/organisms/header'
+import Heading02 from '../components/atoms/Heading02'
 import UtilityButton from '../components/atoms/button'
 import Textarea from '../components/atoms/Textarea'
+import Calender from '../components/Calender'
 
 const App: React.FC = () => {
   const weekDayArr = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
@@ -18,57 +20,13 @@ const App: React.FC = () => {
 
     return `${year}/${month}/${day}(${weekDayArr[weekDayIndex]})`
   }
-  const captionDate = () => {
-    const date = new Date()
-    const year = date.getFullYear()
-    const month = date.getMonth()
-
-    return `${year}/${month}`
-  }
-  const drawingCalendar = (year?: number, month?: number): (string | number)[][] => {
-    const date = new Date()
-    const currentYear = year || date.getFullYear()
-    const currentMonth = month || date.getMonth()
-    const isLeapYear =
-      (currentYear % 4 === 0 && currentYear % 100 !== 0) ||
-      currentYear % 400 ===0
-    const startDate = new Date(currentYear, currentMonth, 1)
-    const startDay = startDate.getDay()
-    const endDate = new Date(currentYear, currentMonth + 1, 0)
-    const endDay = endDate.getDay()
-    const end = endDate.getDate()
-    const dateArr = []
-    let row
-    let col
-    let day = 1
-
-    for (row = 0; row < 6; row++) {
-      const weekArr = []
-      for(col = 0; col < 7; col++) {
-        if (row === 0 && startDay > col) {
-          weekArr.push('')
-        } else if (day > end) {
-          weekArr.push('')
-          day++
-        } else {
-          weekArr.push(day)
-          day++
-        }
-      }
-
-      dateArr.push(weekArr)
-    }
-
-    return dateArr
-  }
-  const calendar = drawingCalendar()
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <div className="App">
         <HeaderContent />
         <main>
-          <h2>{formattedDateStr()}</h2>
+          <Heading02 text={formattedDateStr()} />
           <p>今日がんばったことを書いてね</p>
           <div className="content-input-work">
             <div className="content-input-work__textarea">
@@ -79,24 +37,7 @@ const App: React.FC = () => {
               <UtilityButton isAnchor text={'あしあと'} href="list" />
             </div>
           </div>
-          <div>
-            <h3>がんばったきせき</h3>
-            <table>
-              <caption>{captionDate()}</caption>
-              <thead>
-                <tr>
-                  {weekDayArr.map((day, index) => <th key={`day_${index}`}>{day}</th>)}
-                </tr>
-              </thead>
-                {calendar.map(week =>
-                  <tr>
-                    {week.map(day => <td>{day}</td>)}
-                  </tr>
-                )}
-              <tbody>
-              </tbody>
-            </table>
-          </div>
+          <Calender />
         </main>
         <footer>
           <small>&copy;2020</small>
