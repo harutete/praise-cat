eb9b1e37561559989d7947377e593355317d1da0
harutete <ai.takeuchi.10@gmail.com>
2020-05-10 00:05:45 +0900

    Fix: 導線追加

--- src/components/Result/index.tsx
+++ src/components/Result/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import img01 from '../../assets/img/praize_01.png'
 //Component
 import Heading02 from '../common/atoms/Heading02'
+import UtilityButton from '../common/atoms/UtilityButton'
 
 const Result = () => {
   return (
@@ -9,6 +10,8 @@ const Result = () => {
       <Heading02 text="お疲れ様" />
       <img src={img01} />
       <p>すごい、えらいね！</p>
+      <UtilityButton isAccent isAnchor href="/" text="もっとほめて" />
+      <UtilityButton isAnchor href="/list" text="あしあと" />
     </>
   )
 }
