63671dabcaa87b89b551df65394f405ce0879923
harutete <ai.takeuchi.10@gmail.com>
2020-05-14 22:52:24 +0900

    Fix: イベントをcontextに追加するところまで

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { withRouter } from 'react-router-dom'
+import { eventContext } from '../../App'
 //Component
 import Heading02 from '../common/atoms/Heading02'
 import Textarea from '../common/atoms/Textarea'
 import ButtonList from './ButtonList'
 
-const Home = () => {
+const Home = withRouter((props) => {
   const formattedDateStr  = () => {
     const date = new Date()
     const year = date.getFullYear()
@@ -15,18 +17,49 @@ const Home = () => {
 
     return `${year}/${month}/${day}(${weekDayArr[weekDayIndex]})`
   }
+  const { events, setEvents } = useContext(eventContext)
+  const addDescription = (event: any) => {
+    event.preventDefault()
+    const textareaCollection = event.target.getElementsByTagName('textarea')
+    const value: string = [...textareaCollection][0].value
+    if (value === '') return
+    const date = new Date()
+    const year = date.getFullYear()
+    const month = date.getMonth()
+    const day = date.getDate()
+    const eventsArr = [...events]
+    const findCurrentData = eventsArr.find(event => event.day === `${year}/${month}/${day}`)
+    if (!!findCurrentData) {
+      const mergedEventsArr = eventsArr.map(event => {
+        if (event.day === findCurrentData.day) {
+          event.descriptions.push(value)
+        }
+        return event
+      })
+      setEvents(mergedEventsArr)
+    } else {
+      eventsArr.push({
+        day: `${year}/${month}/${day}`,
+        descriptions: [value]
+      })
+      setEvents(eventsArr)
+    }
+
+    props.history.push('/result')
+  }
   return (
     <>
       <Heading02 text={formattedDateStr()} />
       <p>今日がんばったことを書いてね</p>
       <div className="content-input-work">
-        <div className="content-input-work__textarea">
+        <form onSubmit={addDescription}>
           <Textarea />
-        </div>
-        <ButtonList />
+          <button type='submit'>ほめて</button>
+        </form>
+          {/* <ButtonList onClick={addDescription} /> */}
       </div>
     </>
   )
-}
+})
 
 export default Home
