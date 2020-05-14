ebcc33065fa28496e4fd8b0401fb42f905fe2f63
harutete <ai.takeuchi.10@gmail.com>
2020-05-15 01:02:00 +0900

    Fix: 型指定見直す用のコメントアウト追加

--- src/App.tsx
+++ src/App.tsx
@@ -12,13 +12,28 @@ import Home from './components/Home'
 import ContentWrapper from './components/common/atoms/ContentWrapper'
 import MainContent from './components/common/atoms/MainContent'
 
+// TODO firebase入れたらこの形にする
+// type EventDetailType = {
+//   month: number,
+//   day: [
+//     {
+//       day: number,
+//       description: string[]
+//     }
+//   ]
+// }
+// type EventType = {
+//   year: number,
+//   month: EventDetailType[]
+// }
+
 type EventType = {
   day: string,
   descriptions: string[]
 }
 
 type UseDataContextType = {
-  events: EventType[] | [],
+  events: EventType[],
   setEvents: (events: EventType[]) => void
 }
 export const eventContext = React.createContext<UseDataContextType>({
@@ -30,7 +45,7 @@ export const eventContext = React.createContext<UseDataContextType>({
 const App: React.FC = () => {
   // TODO Firebase入れるときに復活させる
   // const [ user, setUser ] = useState('')
-  const [ events, setEvents ] = useState<EventType[] | []>([])
+  const [ events, setEvents ] = useState<EventType[]>([])
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
