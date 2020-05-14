0a1d961af42655248e78f741ef1085db71d20b6d
harutete <ai.takeuchi.10@gmail.com>
2020-05-14 23:03:53 +0900

    Fix: 投稿した出来事の数によって表示させるテキストと画像を変える

--- src/components/Result/ResultMain.tsx
+++ src/components/Result/ResultMain.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import { userDataContext } from '../../App'
+import { eventContext } from '../../App'
 
 //Component
 import img01 from '../../assets/img/praize_01.png'
@@ -10,12 +10,38 @@ import UtilityButton from '../common/atoms/UtilityButton'
 import ResultImage from './ResultImage'
 
 const Result = () => {
-  const consume = useContext(userDataContext)
+  const { events } = useContext(eventContext)
+  const switchImagePath = () => {
+    const date = new Date()
+    const year = date.getFullYear()
+    const month = date.getMonth()
+    const day = date.getDate()
+    const today = `${year}/${month}/${day}`
+    const todayEvents = events.filter(event => event.day === today)
+    const eventCount = !!todayEvents ? todayEvents[0].descriptions.length : 0
+
+    if (eventCount > 5) {
+      return {
+        text: '神',
+        imagePath: img03
+      }
+    }　else if (eventCount >= 3 && eventCount <= 5) {
+      return {
+        text: 'やばい',
+        imagePath: img02
+      }
+    } else {
+      return {
+        text: 'すごい、えらいね！',
+        imagePath: img01
+      }
+    }
+  }
   return (
     <>
       <Heading02 text="お疲れ様" />
-      <ResultImage src={img01} />
-      <p>すごい、えらいね！</p>
+      <ResultImage src={switchImagePath().imagePath} />
+      <p>{switchImagePath().text}</p>
       <UtilityButton isAccent isAnchor href="/" text="もっとほめて" />
       <UtilityButton isAnchor href="/list" text="あしあと" />
     </>
